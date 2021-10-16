a7b1813dd48a01258226be8e5d248b898f935ce9
ecarbono <ecarbono8@gmail.com>
2021-10-16 13:12:02 -0500

    GG

--- lib/main.dart
+++ lib/main.dart
@@ -14,44 +14,46 @@ class Screan extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Ejercicio'),
         ),
-        body: Center(
-          child: Container(
-            width: 300,
-            height: 150,
-            transform: Matrix4.rotationZ(0.2),
-            margin: const EdgeInsets.symmetric(vertical: 20, horizontal: 10),
-            decoration: BoxDecoration(
-              color: Colors.red[900],
-              border: Border.all(
-                  color: const Color.fromARGB(255, 255, 255, 5),
-                  width: 2,
-                  style: BorderStyle.solid),
-              borderRadius: const BorderRadius.all(Radius.circular(15)),
-              boxShadow: const [
-                BoxShadow(
-                  color: Colors.black,
-                  offset: Offset(6, 6),
-                  blurRadius: 2.5,
-                )
-              ],
-            ),
-            child: Text(
-              'Texto ' * 49,
-              textAlign: TextAlign.center,
-              style: const TextStyle(
-                color: Colors.white,
-                fontSize: 15,
-                fontWeight: FontWeight.bold,
-                fontStyle: FontStyle.normal,
-                decoration: TextDecoration.underline,
-                decorationStyle: TextDecorationStyle.dashed,
-                shadows: [
-                  Shadow(
-                    color: Colors.grey,
-                    offset: Offset(3, 3),
-                    blurRadius: 1,
-                  ),
+        body: SafeArea(
+          child: Center(
+            child: Container(
+              width: 300,
+              height: 150,
+              transform: Matrix4.rotationZ(0.2),
+              margin: const EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+              decoration: BoxDecoration(
+                color: Colors.red[900],
+                border: Border.all(
+                    color: const Color.fromARGB(255, 255, 255, 5),
+                    width: 2,
+                    style: BorderStyle.solid),
+                borderRadius: const BorderRadius.all(Radius.circular(15)),
+                boxShadow: const [
+                  BoxShadow(
+                    color: Colors.black,
+                    offset: Offset(6, 6),
+                    blurRadius: 2.5,
+                  )
                 ],
+              ),
+              child: Text(
+                'Texto ' * 49,
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold,
+                  fontStyle: FontStyle.normal,
+                  decoration: TextDecoration.underline,
+                  decorationStyle: TextDecorationStyle.dashed,
+                  shadows: [
+                    Shadow(
+                      color: Colors.grey,
+                      offset: Offset(3, 3),
+                      blurRadius: 1,
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
